f3c253a5ae344660f61cb5285a9cf6c0206ce255
julietapinto <jjuliepinto@gmail.com>
2026-05-24 22:09:29 -0600

    Backend Cloud
    frontend conectado a backend cloud

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:5000";
+const API = "https://registro-visitantes-wt8j.onrender.com";
 
 const formulario = document.getElementById("formulario");
 const tabla = document.getElementById("tablaVisitantes");
